14b5af55960767526cee0a1ffc9f9f593876d2e4
tamon-persol <60681425+tamon-persol@users.noreply.github.com>
2021-03-19 13:32:50 +0900

    remove .nuxt

--- .nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ToolBar } from '../../components/ToolBar.vue'
-
-export const LazyToolBar = import('../../components/ToolBar.vue' /* webpackChunkName: "components/ToolBar" */).then(c => c.default || c)
